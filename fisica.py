aa5205415b05eb849a439c7e8705424c5f483fbf
Mary026 <128990569+Mary026@users.noreply.github.com>
2023-04-03 20:55:14 +0000

    Parte1

--- fisica.py
+++ fisica.py
@@ -3,6 +3,6 @@ arquivo = open("PosicaoBolaEmY.txt", "r")
 leitura_do_arquivo_Y = arquivo.readlines()
 for linha_arquivo_Y in leitura_do_arquivo_Y:
     arquivo.close()
-    separacao = float(linha_arquivo_Y[:-2])
-    lista_Y.append(separacao)
+    separacao_Y = float(linha_arquivo_Y[:-2])
+    lista_Y.append(separacao_Y)
 print(lista_Y)
