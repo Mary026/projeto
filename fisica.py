d865738bece1413effdf1ad0527276cb9f92c276
Mary026 <128990569+Mary026@users.noreply.github.com>
2023-04-03 20:59:29 +0000

    ListaX

--- fisica.py
+++ fisica.py
@@ -1,10 +1,17 @@
 lista_Y = []
-arquivo = open("PosicaoBolaEmY.txt", "r")
-leitura_do_arquivo_Y = arquivo.readlines()
+arquivo_Y = open("PosicaoBolaEmY.txt", "r")
+leitura_do_arquivo_Y = arquivo_Y.readlines()
 for linha_arquivo_Y in leitura_do_arquivo_Y:
-    arquivo.close()
+    arquivo_Y.close()
     separacao_Y = float(linha_arquivo_Y[:-2])
     lista_Y.append(separacao_Y)
 print(lista_Y)
 
 lista_X = []
+arquivo_X = open("PosicaoBolaEmX.txt", "r")
+leitura_do_arquivo_X = arquivo_X.readlines()
+for linha_arquivo_X in leitura_do_arquivo_X:
+    arquivo_X.close()
+    separacao_X = float(linha_arquivo_X[:-2])
+    lista_X.append(separacao_X)
+print(lista_X)
