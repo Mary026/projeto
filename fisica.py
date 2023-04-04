91e16a02b12b0725dab58d6becb22236e82a8a2b
Mary026 <128990569+Mary026@users.noreply.github.com>
2023-04-04 17:45:35 -0300

    Tirando as virgulas

--- fisica.py
+++ fisica.py
@@ -7,7 +7,7 @@ arquivo_Y = open("PosicaoBolaEmY.txt", "r")
 leitura_do_arquivo_Y = arquivo_Y.readlines()
 for linha_arquivo_Y in leitura_do_arquivo_Y:
     arquivo_Y.close()
-    separacao_Y = float(linha_arquivo_Y[:-2])
+    separacao_Y = float(linha_arquivo_Y[:-1])
     lista_Y.append(separacao_Y)
 print(lista_Y)
 
@@ -16,7 +16,7 @@ arquivo_X = open("PosicaoBolaEmX.txt", "r")
 leitura_do_arquivo_X = arquivo_X.readlines()
 for linha_arquivo_X in leitura_do_arquivo_X:
     arquivo_X.close()
-    separacao_X = float(linha_arquivo_X[:-2])
+    separacao_X = float(linha_arquivo_X[:-1])
     lista_X.append(separacao_X)
 print(lista_X)
 
@@ -25,6 +25,6 @@ arquivo_T = open("TempoBola.txt", "r")
 leitura_do_arquivo_T = arquivo_T.readlines()
 for linha_arquivo_T in leitura_do_arquivo_T:
     arquivo_T.close()
-    separacao_T = float(linha_arquivo_T[:-2])
+    separacao_T = float(linha_arquivo_T[:-1])
     lista_T.append(separacao_T)
 print(lista_T)
