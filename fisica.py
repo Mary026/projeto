f0faf5304901a679061fb866dc0f4d81cb0af2c5
Mary026 <128990569+Mary026@users.noreply.github.com>
2023-04-05 16:16:15 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -1,7 +1,8 @@
+#Inputs para a posição do robô em X e Y
 x = float(input("Digite a posição do robô em X: "))
 y = float(input("Digite a posição do robô em Y: "))
 
-
+#Armazenar os dados da bola na Posição Y do plano cartesiano em uma lista
 lista_Y = []
 arquivo_Y = open("PosicaoBolaEmY.txt", "r")
 leitura_do_arquivo_Y = arquivo_Y.readlines()
@@ -11,6 +12,7 @@ for linha_arquivo_Y in leitura_do_arquivo_Y:
     lista_Y.append(separacao_Y)
 print(lista_Y)
 
+#Armazenar os dados da bola na Posição X do plano cartesiano em uma lista
 lista_X = []
 arquivo_X = open("PosicaoBolaEmX.txt", "r")
 leitura_do_arquivo_X = arquivo_X.readlines()
@@ -20,6 +22,7 @@ for linha_arquivo_X in leitura_do_arquivo_X:
     lista_X.append(separacao_X)
 print(lista_X)
 
+#Armazenar os dados do tempo da bola
 lista_T = []
 arquivo_T = open("TempoBola.txt", "r")
 leitura_do_arquivo_T = arquivo_T.readlines()
@@ -28,3 +31,6 @@ for linha_arquivo_T in leitura_do_arquivo_T:
     separacao_T = float(linha_arquivo_T[:-1])
     lista_T.append(separacao_T)
 print(lista_T)
+
+#Analisando qual posição da bola estará mais perto do robô
+for posicao_mais_perto in lista_X:
