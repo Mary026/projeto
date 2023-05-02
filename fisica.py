874b21c9ad9ec45c9abba2ad71fb1a0209351111
Mary026 <128990569+Mary026@users.noreply.github.com>
2023-05-02 18:02:09 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -1,3 +1,4 @@
+from math import *
 #Inputs para a posição do robô em X e Y
 robo_x = float(input("Digite a posição do robô em X: "))
 robo_y = float(input("Digite a posição do robô em Y: "))
@@ -15,7 +16,6 @@ for linha_arquivo_Y in leitura_do_arquivo_Y:
     arquivo_Y.close()
     separacao_Y = float(linha_arquivo_Y[:-1])
     lista_Y.append(separacao_Y)
-print(lista_Y)
 
 #Armazenar os dados da bola na Posição X do plano cartesiano em uma lista
 lista_X = []
@@ -25,21 +25,33 @@ for linha_arquivo_X in leitura_do_arquivo_X:
     arquivo_X.close()
     separacao_X = float(linha_arquivo_X[:-1])
     lista_X.append(separacao_X)
-print(lista_X)
 
 #Armazenar os dados do tempo da bola
-lista_T = []
-arquivo_T = open("TempoBola.txt", "r")
-leitura_do_arquivo_T = arquivo_T.readlines()
-for linha_arquivo_T in leitura_do_arquivo_T:
-    arquivo_T.close()
-    separacao_T = float(linha_arquivo_T[:-1])
-    lista_T.append(separacao_T)
-print(lista_T)
+#lista_T = []
+#arquivo_T = open("TempoBola.txt", "r")
+#leitura_do_arquivo_T = arquivo_T.readlines()
+#for linha_arquivo_T in leitura_do_arquivo_T:
+#    arquivo_T.close()
+#    separacao_T = float(linha_arquivo_T[:-1])
+#    lista_T.append(separacao_T)
+#print(lista_T)
 
-#Analisando qual posição da bola estará mais perto do robô
-for posicao_mais_perto in lista_X:
-    #if posicao_mais_perto robo_x:
-        print(posicao_mais_perto)
+#Quadrantes - 1 Quadrante
+y_inicial_1 = 3
+y_final_1 = 6
+x_inicial_1 = 4.5
+x_final_1 = 9
 
-#+
+#Quadrantes - 2 Quadrante
+
+#Quadrantes - 3 Quadrante
+#Quadrantes - 4 Quadrante
+#Analisando qual posição da bola estará mais perto do robô em Y
+for posicao_mais_perto_y in lista_Y:
+    if posicao_mais_perto_y < robo_y:
+        posicao_y = posicao_mais_perto_y
+print(posicao_y)
+
+distancia_robo_e_bola = ()
+distancia_robo_e_bola = sqrt(distancia_robo_e_bola)
+print(distancia_robo_e_bola)
